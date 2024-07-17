40e716bdef212ac35253a4002eac7560cf48455e
Danieemen <danieljose1517@gmail.com>
2024-07-17 14:49:24 -0600

    ejemplo 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,58 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
+const Equipos = ({ equipos }) => {
+return (
+<div className={styles.container__list}>
+<h2 className={styles.title}>Equipos de Fútbol</h2>
+{equipos.map((equipo) => (
+<div key={equipo.id}>
+<h3 className={styles.nameclub}>{equipo.nombre}</h3>
+<ul >
+{equipo.plantilla.map((jugador) => (
+<li className={styles.container__list} key={jugador.id}>
+<strong>{jugador.nombre}</strong>
+<p>Altura: {jugador.Altura}m <br></br> Peso:
+{jugador.Peso}Kg <br></br>edad: {jugador.edad}</p>
+</li>
+))}
+</ul>
+</div>
+))}
+</div>
+);
+};
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+// Simula la obtención de datos desde tu JSON
+const equiposData = [
+{"id": 1,
+"nombre": "Real Madrid",
+"plantilla":[
+{"id": 1,"nombre":"Eden Hazard","Altura":"1.75","Peso":"74","edad":"25"},
+{"id": 2,"nombre":"Gonzalo García","Altura":"1.82","Peso":"74","edad":"26"},
+{"id": 3,"nombre":"Karim Benzema","Altura":"1.85","Peso":"81","edad":"35"}
+]},
+{"id": 2,
+"nombre": "Barcelona",
+"plantilla":[
+{"id": 1,"nombre":"Marc-André ter Stegen","Altura":"1.75","Peso":"74Kg","edad":"25"},
+{"id": 2,"nombre":"Iñigo Martinez","Altura":"1.82","Peso":"74Kg","edad":"29"},
+{"id": 3,"nombre":"Gavi","Altura":"1.85","Peso":"81Kg","edad":"17"}
+]},
+{"id": 3,
+"nombre": "Manchester City",
+"plantilla":[
+{"id": 1,"nombre":"Phil Foden","Altura":"1.75","Peso":"74Kg","edad":"21"},
+{"id": 2,"nombre":"Erling Haaland","Altura":"1.92","Peso":"74Kg","edad":"22"},
+{"id": 3,"nombre":"Bernardo Silva","Altura":"1.85","Peso":"81Kg","edad":"15"}
+]}
+// ... agregar otros equipos
+];
+return (
+<main className={styles.main}>
+<div>
+<h1>Mi Aplicación de Fútbol</h1>
+<Equipos equipos={equiposData} />
+</div>
+</main>
+);
 }
+
